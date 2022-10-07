f84761aa1fa6e60764d4099dc331d21bbe0b8651
EugenePltntsk <eugeneplotnytskyi@gmail.com>
2022-10-07 17:01:30 +0300

    search by letter in li done

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -3,7 +3,7 @@ import { Notify } from "notiflix";
 
 export const searchCoctails = async (coctailName) => {
 const response  = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?key=1&s=${coctailName}`)
-console.log(response);
+// console.log(response);
 if (response.status >= 400) {
     Notify.failure("Something is wrong...")
     return [];
@@ -14,4 +14,15 @@ if (response.status >= 400) {
 return response.data.drinks;
 }
 
-
+export const searchByFirstLetter = async (letter) => {
+    const response  = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?key=1&f=${letter}`)
+console.log(response);
+if (response.status >= 400) {
+    Notify.failure("Something is wrong...")
+    return [];
+} else if (!response.data.drinks) {
+    Notify.warning("There's no drink with such name");
+    return [];
+}
+return response.data.drinks;
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import { Notify } from "notiflix";
-import { searchCoctails } from "./helpers/api";
+import { searchCoctails, searchByFirstLetter } from "./helpers/api";
+import { getAlphabetMarkup } from "./helpers/helpers"
 
 const searchForm = document.querySelector("#form");
 const searchField = document.querySelector("#input");
 const searchBtn = document.querySelector("#button");
 const gallery = document.querySelector("#gallery");
+const alphabetUl = document.querySelector("#alphabet");
+
 
 // console.log(form);
+
+
+
+getAlphabetMarkup(alphabetUl);
 
 searchForm.addEventListener("submit", async event => {
     event.preventDefault();
@@ -21,4 +28,19 @@ searchForm.addEventListener("submit", async event => {
         return `<li><a><img src="${strDrinkThumb}" alt="${strDrink}"/><h3>${strDrink}</h3><button>Learn more</button><button id="${idDrink}">Add to</button></a></li>`
     })
    gallery.innerHTML = template.join("");
+})
+
+
+alphabetUl.addEventListener("click", async event => {
+    if(event.target.classList.contains("letterInLi")) {
+        const resultLetter = event.target.textContent;
+        const drinks = await searchByFirstLetter(resultLetter);
+
+        const template = drinks.map(({ idDrink, strDrink, strDrinkThumb }) => {
+            return `<li><a><img src="${strDrinkThumb}" alt="${strDrink}"/><h3>${strDrink}</h3><button>Learn more</button><button id="${idDrink}">Add to</button></a></li>`
+        })
+       gallery.innerHTML = template.join("");
+
+        
+    }
 })
