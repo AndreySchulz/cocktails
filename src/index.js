10665942ea06b771cf85680b59ebb9686446db8c
MarinaPolich <marina.polich@gmail.com>
2022-10-08 15:58:40 +0300

    addFavorites and removeFavorites

--- src/index.js
+++ src/index.js
@@ -1,46 +1,63 @@
-import { Notify } from "notiflix";
-import { searchCoctails, searchByFirstLetter } from "./helpers/api";
-import { getAlphabetMarkup } from "./helpers/helpers"
+import { Notify } from 'notiflix';
+import { searchCoctails, searchByFirstLetter } from './helpers/api';
+import { getAlphabetMarkup } from './helpers/helpers';
+import { addCoctailToFavorites, isCoctailFavorites, removeCoctailFromFavorites } from './js/favorites';
 
-const searchForm = document.querySelector("#form");
-const searchField = document.querySelector("#input");
-const searchBtn = document.querySelector("#button");
-const gallery = document.querySelector("#gallery");
-const alphabetUl = document.querySelector("#alphabet");
-
+const searchForm = document.querySelector('#form');
+const searchField = document.querySelector('#input');
+const searchBtn = document.querySelector('#button');
+const gallery = document.querySelector('#gallery');
+const alphabetUl = document.querySelector('#alphabet');
 
 // console.log(form);
 
-
-
 getAlphabetMarkup(alphabetUl);
 
-searchForm.addEventListener("submit", async event => {
-    event.preventDefault();
-    const coctailName = searchField.value.trim();
-    if (coctailName === "") {
-        Notify.info("Please enter the name of your coctail");
-        return;
-    }
-    const drinks = await searchCoctails(coctailName);
-       
+searchForm.addEventListener('submit', async event => {
+  event.preventDefault();
+  const coctailName = searchField.value.trim();
+  if (coctailName === '') {
+    Notify.info('Please enter the name of your coctail');
+    return;
+  }
+  const drinks = await searchCoctails(coctailName);
+
+  const template = drinks.map(({ idDrink, strDrink, strDrinkThumb }) => {
+    return `<li><a><img src="${strDrinkThumb}" alt="${strDrink}"/><h3>${strDrink}</h3><button>Learn more</button>${getFavoriteBtn(
+      idDrink
+    )}</a></li>`;
+  });
+  gallery.innerHTML = template.join('');
+});
+
+alphabetUl.addEventListener('click', async event => {
+  if (event.target.classList.contains('letterInLi')) {
+    const resultLetter = event.target.textContent;
+    const drinks = await searchByFirstLetter(resultLetter);
+
     const template = drinks.map(({ idDrink, strDrink, strDrinkThumb }) => {
-        return `<li><a><img src="${strDrinkThumb}" alt="${strDrink}"/><h3>${strDrink}</h3><button>Learn more</button><button id="${idDrink}">Add to</button></a></li>`
-    })
-   gallery.innerHTML = template.join("");
-})
+      return `<li><a><img src="${strDrinkThumb}" alt="${strDrink}"/><h3>${strDrink}</h3><button>Learn more</button>${getFavoriteBtn(
+        idDrink
+      )}</a></li>`;
+    });
+    gallery.innerHTML = template.join('');
+  }
+});
 
+gallery.addEventListener('click', event => {
+  const addBtn = event.target.closest('[data-add-favorite]');
+  if (addBtn) {
+    addCoctailToFavorites(addBtn.id);
+  }
+  const removeBtn = event.target.closest('[data-remove-favorite]');
+  if (removeBtn) {
+    removeCoctailFromFavorites(removeBtn.id);
+  }
+});
 
-alphabetUl.addEventListener("click", async event => {
-    if(event.target.classList.contains("letterInLi")) {
-        const resultLetter = event.target.textContent;
-        const drinks = await searchByFirstLetter(resultLetter);
+function getFavoriteBtn(id) {
+  return isCoctailFavorites(id)
+    ? `<button id="${id}" data-remove-favorite>Remove</button>`
+    : `<button id="${id}" data-add-favorite>Add to</button>`;
+}
 
-        const template = drinks.map(({ idDrink, strDrink, strDrinkThumb }) => {
-            return `<li><a><img src="${strDrinkThumb}" alt="${strDrink}"/><h3>${strDrink}</h3><button>Learn more</button><button id="${idDrink}">Add to</button></a></li>`
-        })
-       gallery.innerHTML = template.join("");
-
-        
-    }
-})
