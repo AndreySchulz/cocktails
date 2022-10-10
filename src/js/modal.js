3764947c1829cceedec789d39d296b9d8dbd6b7a
MarinaPolich <marina.polich@gmail.com>
2022-10-11 00:02:43 +0300

    fix buf

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,32 +1,48 @@
+const gallery = document.querySelector('#gallery');
+
 function createOnClickForModal(addToFavorites, removeFromFavorites) {
-    return async  function(event) {
-        const backDrop = event.currentTarget;
-        if (event.target == backDrop) {
-            backDrop.classList.add('is-hidden');
-            return;
-          }
-          const closeBtn = event.target.closest('[data-modal-close]');
-          if (closeBtn) {
-            backDrop.classList.add('is-hidden');
-            return;
-          }
-          const addFavoriteBtn = event.target.closest('[data-add-favorite]');
-          if (addFavoriteBtn) {
-            addToFavorites(addFavoriteBtn.id);
-            addFavoriteBtn.classList.add('is-hidden');
-            addFavoriteBtn.parentNode
-              .querySelector('[data-remove-favorite]')
-              .classList.remove('is-hidden');
-          }
-          const removeFavoriteBtn = event.target.closest('[data-remove-favorite]');
-          if (removeFavoriteBtn) {
-            removeFromFavorites(removeFavoriteBtn.id);
-            removeFavoriteBtn.classList.add('is-hidden');
-            removeFavoriteBtn.parentNode
-              .querySelector('[data-add-favorite]')
-              .classList.remove('is-hidden');
-          }
+  return async function (event) {
+    const backDrop = event.currentTarget;
+    if (event.target == backDrop) {
+      backDrop.classList.add('is-hidden');
+      return;
     }
+    const closeBtn = event.target.closest('[data-modal-close]');
+    if (closeBtn) {
+      backDrop.classList.add('is-hidden');
+      return;
+    }
+    const addFavoriteBtn = event.target.closest('[data-add-favorite]');
+    if (addFavoriteBtn) {
+      addToFavorites(addFavoriteBtn.id);
+      addFavoriteBtn.classList.add('is-hidden');
+      addFavoriteBtn.parentNode
+        .querySelector('[data-remove-favorite]')
+        .classList.remove('is-hidden');
+
+      gallery
+        .querySelector(`[id='${addFavoriteBtn.id}'][data-add-favorite]`)
+        ?.classList.add('is-hidden');
+      gallery
+        .querySelector(`[id='${addFavoriteBtn.id}'][data-remove-favorite]`)
+        ?.classList.remove('is-hidden');
+    }
+    const removeFavoriteBtn = event.target.closest('[data-remove-favorite]');
+    if (removeFavoriteBtn) {
+      removeFromFavorites(removeFavoriteBtn.id);
+      removeFavoriteBtn.classList.add('is-hidden');
+      removeFavoriteBtn.parentNode
+        .querySelector('[data-add-favorite]')
+        .classList.remove('is-hidden');
+
+      gallery
+        .querySelector(`[id='${removeFavoriteBtn.id}'][data-add-favorite]`)
+        ?.classList.remove('is-hidden');
+      gallery
+        .querySelector(`[id='${removeFavoriteBtn.id}'][data-remove-favorite]`)
+        ?.classList.add('is-hidden');
+    }
+  };
 }
 
 export { createOnClickForModal };
